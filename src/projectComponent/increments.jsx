608bbe0c254045a7e5eaafb65f0c9d6ee2dc87f9
Md Abu Taleb <sumon.pdk.bd@gmail.com>
2020-04-01 13:52:34 +0600

    Arguments descructing

--- src/projectComponent/increments.jsx
+++ src/projectComponent/increments.jsx
@@ -4,16 +4,17 @@ import Increment from "./increment";
 class Increments extends Component {
 
     render() {
+        const {onReset, onDelete, counters, onIncrement} = this.props;
         return (
             <div>
-                <button className="btn btn-secondary btn-sm m-2" onClick={this.props.onReset}>Reset</button>
+                <button className="btn btn-secondary btn-sm m-2" onClick={onReset}>Reset</button>
                 {
-                    this.props.counters.map(counter => (
+                    counters.map(counter => (
                             <Increment
                                 key={counter.id}
                                 counter={counter}
-                                onDelete={this.props.onDelete}
-                                onIncrement={this.props.onIncrement}
+                                onDelete={onDelete}
+                                onIncrement={onIncrement}
                             >
                             </Increment>
 
--- src/projectComponent/navbar.jsx
+++ src/projectComponent/navbar.jsx
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 
-class Navbar extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <a className="navbar-brand" href="#">Navbar <span className="badge badge-primary">{this.props.totalCount}</span> </a>
-                </nav>
-            </React.Fragment>
-        );
-    }
+const Navbar = (props) => {
+
+    return (
+        <React.Fragment>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">Navbar <span
+                    className="badge badge-primary">{props.totalCount}</span> </a>
+            </nav>
+        </React.Fragment>
+    );
+
 }
 
 export default Navbar;
