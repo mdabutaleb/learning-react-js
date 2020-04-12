e65252114c0cbb563b8ff8d880d7e1243f98ae63
Md Abu Taleb <sumon.pdk.bd@gmail.com>
2020-04-12 16:35:00 +0600

    Managing unexpected error globally

--- src/projectComponent/admin/posts/posts.jsx
+++ src/projectComponent/admin/posts/posts.jsx
@@ -8,6 +8,15 @@ import PaginationButton from "../../../utilis/paginationButton";
 
 const apiEndPoint = 'https://jsonplaceholder.typicode.com/posts';
 
+axios.interceptors.response.use(null, error => {
+    const expectedError = error.response && error.response.status >= 400 && error
+    if (!expectedError) {
+        console.log("Logging the error", error);
+        alert('An unexpected error occurred!')
+    }
+    return Promise.reject(error);
+});
+
 class Posts extends Component {
     constructor(props) {
         super(props);
@@ -23,22 +32,22 @@ class Posts extends Component {
         this.setState({
             data
         })
-        console.log(data)
+        // console.log(data)
     }
 
     handleAdd = async () => {
-        const item = {title: "Sumon Mahmud", body: "This is body"}
-        const {data} = await axios.post(apiEndPoint, item);
-        const posts = [data, ...this.state.data];
+        const item = {title: "Sumon Mahmud", body: "This is body", id: 202}
+        const posts = [item, ...this.state.data];
         this.setState({data: posts})
+        const {data} = await axios.post(apiEndPoint, item);
     }
     handleUpdate = async item => {
         item.title = "Updated by Sumon Mahmud"
-        const {data} = await axios.put(apiEndPoint + '/' + item.id, item);
         const posts = [...this.state.data];
         const index = posts.indexOf(item);
-        posts[index] = {...data}
+        posts[index] = {...item}
         this.setState({data: posts})
+        const {data} = await axios.put(apiEndPoint + '/' + item.id, item);
     }
 
     handleDelete = async item => {
@@ -51,10 +60,6 @@ class Posts extends Component {
         } catch (error) {
             if (error.response && error.response.status == 404)
                 alert('This post has already been deleted!')
-            else {
-                alert('An unexpected error occur');
-            }
-
             this.setState({data: originalData})
         }
 
